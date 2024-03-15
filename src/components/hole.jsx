7e148812886b662c88b37090cb322a99a5578485
nathan-burd <nathanburd.dev@gmail.com>
2024-03-14 23:22:35 -0400

    created files for course / range components

---
+++ src/components/hole.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const hole = () => {
+  return (
+    <View>
+      <Text>hole</Text>
+    </View>
+  )
+}
+
+export default hole
+
+const styles = StyleSheet.create({})
