ffd835fa6b1dc342ff1e081481ce41e24439878a
nathan-burd <nathanburd.dev@gmail.com>
2024-03-16 23:42:31 -0400

    created currentHole functional component

---
+++ src/components/buttons/currentHole.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const currentHole = () => {
+  return (
+    <View>
+      <Text>currentHole</Text>
+    </View>
+  )
+}
+
+export default currentHole
+
+const styles = StyleSheet.create({})
