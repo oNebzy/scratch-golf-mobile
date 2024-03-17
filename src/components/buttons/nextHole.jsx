715f2e9b92af23ad104773d9b871d3b5cc889022
nathan-burd <nathanburd.dev@gmail.com>
2024-03-16 23:43:05 -0400

    created hole navigation button functional components

---
+++ src/components/buttons/nextHole.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const nextHole = () => {
+  return (
+    <View>
+      <Text>nextHole</Text>
+    </View>
+  )
+}
+
+export default nextHole
+
+const styles = StyleSheet.create({})
---
+++ src/components/buttons/prevHole.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const prevHole = () => {
+  return (
+    <View>
+      <Text>prevHole</Text>
+    </View>
+  )
+}
+
+export default prevHole
+
+const styles = StyleSheet.create({})
