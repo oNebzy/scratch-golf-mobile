779f9247fea5bce3ded8870d56901cd9552d3165
nathan-burd <nathanburd.dev@gmail.com>
2024-03-16 23:43:39 -0400

    created range object functional component

---
+++ src/components/Course/rangeTarget.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const rangeTarget = () => {
+  return (
+    <View>
+      <Text>rangeTarget</Text>
+    </View>
+  )
+}
+
+export default rangeTarget
+
+const styles = StyleSheet.create({})
