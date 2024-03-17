9b849d80f4be16302d4ed91d4ff038ced4ba0c6e
nathan-burd <nathanburd.dev@gmail.com>
2024-03-16 23:43:21 -0400

    created fairway object functional component

---
+++ src/components/Course/fairway.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const fairway = () => {
+  return (
+    <View>
+      <Text>fairway</Text>
+    </View>
+  )
+}
+
+export default fairway
+
+const styles = StyleSheet.create({})
