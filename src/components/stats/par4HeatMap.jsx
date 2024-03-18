2cce5e770cfa448a4f1a6adcc0c70535151119f1
nathan-burd <nathanburd.dev@gmail.com>
2024-03-17 23:40:38 -0400

    created par 4 hole heat map stat component

---
+++ src/components/stats/par4HeatMap.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const par4HeatMap = () => {
+  return (
+    <View>
+      <Text>par4HeatMap</Text>
+    </View>
+  )
+}
+
+export default par4HeatMap
+
+const styles = StyleSheet.create({})
