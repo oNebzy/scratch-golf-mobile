5d4cc60c561d4197e55b7857021ae71a5c452b0f
nathan-burd <nathanburd.dev@gmail.com>
2024-03-17 23:40:21 -0400

    created green heat map stat component

---
+++ src/components/stats/greenHeatMap.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const greenHeatMap = () => {
+  return (
+    <View>
+      <Text>greenHeatMap</Text>
+    </View>
+  )
+}
+
+export default greenHeatMap
+
+const styles = StyleSheet.create({})
