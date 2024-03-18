30fe9b5e392151f0761c005f1045f5b483b406e8
nathan-burd <nathanburd.dev@gmail.com>
2024-03-17 23:40:44 -0400

    created par 5 hole heat map stat component

---
+++ src/components/stats/par5HeatMap.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const par5HeatMap = () => {
+  return (
+    <View>
+      <Text>par5HeatMap</Text>
+    </View>
+  )
+}
+
+export default par5HeatMap
+
+const styles = StyleSheet.create({})
