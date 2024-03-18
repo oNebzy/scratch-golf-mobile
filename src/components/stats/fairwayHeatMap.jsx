7e4ce8d9b10293dc5472c51ac72f4b473e0f34c7
nathan-burd <nathanburd.dev@gmail.com>
2024-03-17 23:40:15 -0400

    created fairway heat map stat component

---
+++ src/components/stats/fairwayHeatMap.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const fairwayHeatMap = () => {
+  return (
+    <View>
+      <Text>fairwayHeatMap</Text>
+    </View>
+  )
+}
+
+export default fairwayHeatMap
+
+const styles = StyleSheet.create({})
