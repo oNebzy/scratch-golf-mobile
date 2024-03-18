c4c6a4fb37a9cbe0967ff0dab386f4441a422bd7
nathan-burd <nathanburd.dev@gmail.com>
2024-03-17 23:41:03 -0400

    created graph type component

---
+++ src/components/stats/pieChart.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PieChart = () => {
+  return (
+    <View>
+      <Text>PieChart</Text>
+    </View>
+  )
+}
+
+export default PieChart
+
+const styles = StyleSheet.create({})
---
+++ src/components/stats/scoreLineGraph.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const scoreLineGraph = () => {
+  return (
+    <View>
+      <Text>scoreLineGraph</Text>
+    </View>
+  )
+}
+
+export default scoreLineGraph
+
+const styles = StyleSheet.create({})
