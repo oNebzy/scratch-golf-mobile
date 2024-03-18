e5797c66512422c49d5a9db3692a6830c1494a6f
nathan-burd <nathanburd.dev@gmail.com>
2024-03-17 23:41:15 -0400

    created range/target heat map stat component

---
+++ src/components/stats/rangeHeatMap.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const rangeHeatMap = () => {
+  return (
+    <View>
+      <Text>rangeHeatMap</Text>
+    </View>
+  )
+}
+
+export default rangeHeatMap
+
+const styles = StyleSheet.create({})
